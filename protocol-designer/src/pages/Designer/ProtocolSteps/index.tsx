498e82d8e5cebaf8c7d38d535a87127adda2c62a
Jethary <jethary@opentrons.com>
2025-03-13 12:49:20 -0400

    refactor(protocol-designer): show timeline errors when a step is opened

--- protocol-designer/src/pages/Designer/ProtocolSteps/index.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/index.tsx
@@ -75,10 +75,9 @@ export function ProtocolSteps(): JSX.Element {
   const { errors: timelineErrors } = useSelector(getRobotStateTimeline)
   const hasTimelineErrors =
     timelineErrors != null ? timelineErrors.length > 0 : false
-  const showTimelineAlerts =
-    hasTimelineErrors && tab === 'protocolSteps' && formData == null
+  const showTimelineAlerts = hasTimelineErrors && tab === 'protocolSteps'
   const stepDetails = currentStep?.stepDetails ?? null
-
+  console.log(showTimelineAlerts, hasTimelineErrors, tab, formData)
   return (
     <Flex
       backgroundColor={COLORS.grey10}
