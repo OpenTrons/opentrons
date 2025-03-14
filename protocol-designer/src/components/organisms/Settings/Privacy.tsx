510bbf8e66b286637f1e2fc67ce96cb61a3e9e58
koji <kk2796@nyu.edu>
2025-03-14 11:28:46 -0400

    switch btn to toggle button

--- protocol-designer/src/components/organisms/Settings/Privacy.tsx
+++ protocol-designer/src/components/organisms/Settings/Privacy.tsx
@@ -1,20 +1,17 @@
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
-import { css } from 'styled-components'
 
 import {
   ALIGN_CENTER,
-  Btn,
-  COLORS,
   DIRECTION_COLUMN,
   Flex,
-  Icon,
   JUSTIFY_SPACE_BETWEEN,
   ListItem,
   SPACING,
   StyledText,
 } from '@opentrons/components'
 
+import { ToggleButton } from '../../atoms/ToggleButton'
 import { actions as analyticsActions } from '../../../analytics'
 
 interface PrivacyProps {
@@ -49,57 +46,16 @@ export function Privacy({ hasOptedIn }: PrivacyProps): JSX.Element {
             {t('shared:we_are_improving')}
           </StyledText>
         </Flex>
-        <Btn
-          role="switch"
-          data-testid="analyticsToggle"
-          size="2rem"
-          css={
-            Boolean(hasOptedIn) ? TOGGLE_ENABLED_STYLES : TOGGLE_DISABLED_STYLES
-          }
-          onClick={() =>
+        <ToggleButton
+          label="Settings_Privacy"
+          toggledOn={Boolean(hasOptedIn)}
+          onClick={() => {
             dispatch(
               hasOptedIn ? analyticsActions.optOut() : analyticsActions.optIn()
             )
-          }
-        >
-          <Icon
-            name={hasOptedIn ? 'ot-toggle-input-on' : 'ot-toggle-input-off'}
-            height="1rem"
-          />
-        </Btn>
+          }}
+        />
       </ListItem>
     </Flex>
   )
 }
-
-const TOGGLE_DISABLED_STYLES = css`
-  color: ${COLORS.grey50};
-
-  &:hover {
-    color: ${COLORS.grey55};
-  }
-
-  &:focus-visible {
-    box-shadow: 0 0 0 3px ${COLORS.yellow50};
-  }
-
-  &:disabled {
-    color: ${COLORS.grey30};
-  }
-`
-
-const TOGGLE_ENABLED_STYLES = css`
-  color: ${COLORS.blue50};
-
-  &:hover {
-    color: ${COLORS.blue55};
-  }
-
-  &:focus-visible {
-    box-shadow: 0 0 0 3px ${COLORS.yellow50};
-  }
-
-  &:disabled {
-    color: ${COLORS.grey30};
-  }
-`
