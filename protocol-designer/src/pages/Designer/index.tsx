d144f30475cb6d388f2f015fc9d9f1262379f602
koji <kk2796@nyu.edu>
2025-03-14 16:35:09 -0400

    fix(protocol-designer): add a new component to render deck view
    add a new component to render deck view
    
    close AUTH-1574

--- protocol-designer/src/pages/Designer/index.tsx
+++ protocol-designer/src/pages/Designer/index.tsx
@@ -3,16 +3,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import {
-  ALIGN_CENTER,
-  ALIGN_STRETCH,
-  COLORS,
   DIRECTION_COLUMN,
   Flex,
   INFO_TOAST,
-  JUSTIFY_CENTER,
-  JUSTIFY_FLEX_END,
-  SPACING,
-  ToggleGroup,
   useOnClickOutside,
 } from '@opentrons/components'
 import {
@@ -28,11 +21,10 @@ import {
 } from '../../components/organisms'
 import { selectDesignerTab } from '../../file-data/actions'
 import { getDesignerTab, getFileMetadata } from '../../file-data/selectors'
-import { DeckSetupContainer } from './DeckSetup'
 import { selectors } from '../../labware-ingred/selectors'
-import { OffDeck } from './OffDeck'
 import { LiquidsOverflowMenu } from './LiquidsOverflowMenu'
 import { ProtocolSteps } from './ProtocolSteps'
+import { ProtocolStartingDeck } from './ProtocolStartingDeck'
 
 import type { CutoutId } from '@opentrons/shared-data'
 import type { DeckSlot } from '@opentrons/step-generation'
@@ -43,11 +35,7 @@ export interface OpenSlot {
 }
 
 export function Designer(): JSX.Element {
-  const { t } = useTranslation([
-    'starting_deck_state',
-    'protocol_steps',
-    'shared',
-  ])
+  const { t } = useTranslation(['starting_deck_state', 'protocol_steps'])
   const { bakeToast, makeSnackbar } = useKitchen()
   const navigate = useNavigate()
   const dispatch = useDispatch()
@@ -58,12 +46,6 @@ export function Designer(): JSX.Element {
   const [liquidOverflowMenu, showLiquidOverflowMenu] = useState<boolean>(false)
   const [showDefineLiquidModal, setDefineLiquidModal] = useState<boolean>(false)
   const tab = useSelector(getDesignerTab)
-  const leftString = t('onDeck')
-  const rightString = t('offDeck')
-
-  const [deckView, setDeckView] = useState<
-    typeof leftString | typeof rightString
-  >(leftString)
 
   const { modules, additionalEquipmentOnDeck } = deckSetup
 
@@ -129,20 +111,6 @@ export function Designer(): JSX.Element {
     },
   })
 
-  const deckViewItems =
-    deckView === leftString ? (
-      <Flex
-        height="100%"
-        width="100%"
-        justifyContent={JUSTIFY_CENTER}
-        alignItems={ALIGN_CENTER}
-      >
-        <DeckSetupContainer tab={tab} />
-      </Flex>
-    ) : (
-      <OffDeck tab={tab} />
-    )
-
   useEffect(() => {
     if (tab === 'startingDeck') {
       //  ensure that the starting deck page is always showing the initial deck setup
@@ -180,47 +148,7 @@ export function Designer(): JSX.Element {
         />
 
         {tab === 'startingDeck' ? (
-          <Flex
-            flexDirection={DIRECTION_COLUMN}
-            backgroundColor={COLORS.grey10}
-            gridGap={SPACING.spacing24}
-            padding={
-              zoomIn.slot != null
-                ? '0'
-                : `${SPACING.spacing60} ${SPACING.spacing40}`
-            }
-            height="100%"
-            width="100%"
-          >
-            <Flex
-              flexDirection={DIRECTION_COLUMN}
-              gridGap={SPACING.spacing24}
-              height="100%"
-            >
-              {zoomIn.slot == null ? (
-                <Flex
-                  justifyContent={JUSTIFY_FLEX_END}
-                  alignItems={ALIGN_CENTER}
-                  alignSelf={ALIGN_STRETCH}
-                  width="100%"
-                  height="2.25rem"
-                >
-                  <ToggleGroup
-                    selectedValue={deckView}
-                    leftText={leftString}
-                    rightText={rightString}
-                    leftClick={() => {
-                      setDeckView(leftString)
-                    }}
-                    rightClick={() => {
-                      setDeckView(rightString)
-                    }}
-                  />
-                </Flex>
-              ) : null}
-              {deckViewItems}
-            </Flex>
-          </Flex>
+          <ProtocolStartingDeck zoomIn={zoomIn} />
         ) : (
           <ProtocolSteps />
         )}
