33a620e25f8abb39376146aca59ec1d0e9a5a53b
koji <kk2796@nyu.edu>
2025-03-24 12:03:44 -0400

    feat(protocol-designer): add liquid classes button as step 2
    add liquid classes button as step 2
    
    close AUTH-970

---
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/StepTools/MixTools/__tests__/MixTools.test.tsx
@@ -0,0 +1,88 @@
+import { describe, it, vi, beforeEach } from 'vitest'
+import { screen } from '@testing-library/react'
+import { fixture96Plate } from '@opentrons/shared-data'
+
+import { renderWithProviders } from '../../../../../../../__testing-utils__'
+import {
+  getLabwareEntities,
+  getPipetteEntities,
+} from '../../../../../../../step-forms/selectors'
+import {
+  getEnablePartialTipSupport,
+  getEnableReturnTip,
+} from '../../../../../../../feature-flags/selectors'
+import { getFormErrorsMappedToField } from '../../../utils'
+import { MixToolsFirstStep } from '../MixToolsFirstStep'
+import { MixToolsSecondStep } from '../MixToolsSecondStep'
+import { MixTools } from '..'
+
+import type { ComponentProps } from 'react'
+import type { LabwareDefinition2 } from '@opentrons/shared-data'
+import type { FieldPropsByName } from '../../../types'
+import type { FormData } from '../../../../../../../form-types'
+import type { StepFormErrors } from '../../../../../../../steplist'
+
+vi.mock('../../../../../../../step-forms/selectors')
+vi.mock('../../../../../../../feature-flags/selectors')
+vi.mock('../../../utils')
+vi.mock('../MixToolsFirstStep')
+vi.mock('../MixToolsSecondStep')
+
+const labwareId = 'mockLabwareId'
+const pipetteId = 'mockPipetteId'
+
+const render = (props: ComponentProps<typeof MixTools>) => {
+  return renderWithProviders(<MixTools {...props} />)
+}
+
+describe('MixToolFirstStep', () => {
+  let props: ComponentProps<typeof MixTools>
+
+  beforeEach(() => {
+    props = {
+      propsForFields: {} as FieldPropsByName,
+      formData: {} as FormData,
+      toolboxStep: 0,
+      visibleFormErrors: {} as StepFormErrors,
+      tab: 'aspirate',
+      setTab: vi.fn(),
+    }
+    vi.mocked(getLabwareEntities).mockReturnValue({
+      labwareId: {
+        id: labwareId,
+        labwareDefURI: 'mockUri',
+        def: fixture96Plate as LabwareDefinition2,
+        pythonName: 'mockPythonName',
+      },
+    })
+    vi.mocked(getPipetteEntities).mockReturnValue({
+      [pipetteId]: {
+        name: 'p50_single_flex',
+        spec: {} as any,
+        id: pipetteId,
+        tiprackLabwareDef: [],
+        tiprackDefURI: ['mockDefURI1', 'mockDefURI2'],
+        pythonName: 'mockPythonName',
+      },
+    })
+    vi.mocked(getEnableReturnTip).mockReturnValue(false)
+    vi.mocked(getEnablePartialTipSupport).mockReturnValue(false)
+    vi.mocked(getFormErrorsMappedToField).mockReturnValue({})
+    vi.mocked(MixToolsFirstStep).mockReturnValue(
+      <div>mock MixToolsFirstStep</div>
+    )
+    vi.mocked(MixToolsSecondStep).mockReturnValue(
+      <div>mock MixToolsSecondStep</div>
+    )
+  })
+
+  it('renders MixToolFirstStep when toolboxStep is 0', () => {
+    render(props)
+    screen.getByText('mock MixToolsFirstStep')
+  })
+  it('renders MixToolsSecondStep when toolboxStep is 1', () => {
+    props.toolboxStep = 1
+    render(props)
+    screen.getByText('mock MixToolsSecondStep')
+  })
+})
