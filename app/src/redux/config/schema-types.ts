bc3aa7e2728f05ed25f9e8dbda8787b12ef005c0
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-03-24 12:36:22 -0400

    feat(app): Quick Transfer export python ff (#17861)

--- app/src/redux/config/schema-types.ts
+++ app/src/redux/config/schema-types.ts
@@ -16,6 +16,7 @@ export type DevInternalFlag =
   | 'enableLabwareCreator'
   | 'reactQueryDevtools'
   | 'reactScan'
+  | 'quickTransferExportPython'
 
 export type FeatureFlags = Partial<Record<DevInternalFlag, boolean | undefined>>
 
