b118839e429c7efcc266ea5f3f746a650642277e
rclarke0 <rhyann.clarke@opentrons.com>
2025-03-28 01:46:10 -0400

    tip pick up test

--- abr-testing/abr_testing/protocols/test_protocols/tip_pick_up_test.py
+++ abr-testing/abr_testing/protocols/test_protocols/tip_pick_up_test.py
@@ -11,6 +11,7 @@ def add_parameters(parameters: ParameterContext) -> None:
     helpers.create_pipette_parameters(parameters)
     helpers.create_tip_size_parameter(parameters)
     helpers.create_single_pipette_mount_parameter(parameters)
+    helpers.create_all_deck_slot_parameters(parameters)
 
 
 def run(protocol: ProtocolContext) -> None:
@@ -19,7 +20,21 @@ def run(protocol: ProtocolContext) -> None:
     pipette_mount = protocol.params.pipette_mount  # type: ignore[attr-defined]
     pipette_type_left = protocol.params.left_mount  # type: ignore[attr-defined]
     pipette_type_right = protocol.params.right_mount  # type: ignore[attr-defined]
-    tip_rack_slots = ["D2", "D3", "C1", "C2", "B2", "A2", "A3"]
+    slot_mapping = {
+        "A1": protocol.params.A1,  # type: ignore[attr-defined]
+        "A2": protocol.params.A2,  # type: ignore[attr-defined]
+        "A3": protocol.params.A3,  # type: ignore[attr-defined]
+        "B1": protocol.params.B1,  # type: ignore[attr-defined]
+        "B2": protocol.params.B2,  # type: ignore[attr-defined]
+        "B3": protocol.params.B3,  # type: ignore[attr-defined]
+        "C1": protocol.params.C1,  # type: ignore[attr-defined]
+        "C2": protocol.params.C2,  # type: ignore[attr-defined]
+        "C3": protocol.params.C3,  # type: ignore[attr-defined]
+        "D1": protocol.params.D1,  # type: ignore[attr-defined]
+        "D2": protocol.params.D2,  # type: ignore[attr-defined]
+        "D3": protocol.params.D3,  # type: ignore[attr-defined]
+    }
+    tip_rack_slots = [slot for slot, value in slot_mapping.items() if value]
     tip_racks = []
     for slot in tip_rack_slots:
         tip_racks.append(protocol.load_labware(tip_rack_type, slot))
